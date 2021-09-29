da4553fe5815cfcdbf84645517a563d7beb44a6a
carmen_gm14 <90892533+carmengm14@users.noreply.github.com>
2021-09-29 08:46:53 +0200

    Add files via upload

---
+++ Ej3.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Ej3{
+	public static void main(String args[]){
+		int squareArea = 5*5;
+		System.out.println("El área de un cuadrado de lado l=5: " + squareArea);
+	}
+}
