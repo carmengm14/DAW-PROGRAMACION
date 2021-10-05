4e506c6c6877fdc305d262d542e09caec7a58674
carmen_gm14 <90892533+carmengm14@users.noreply.github.com>
2021-10-05 22:26:53 +0200

    Add files via upload

--- Ejer16.java
+++ Ejer16.java
@@ -2,37 +2,31 @@ import java.util.Scanner;
 
 public class Ejer16 {
     public static void main(String args[]){
-    
-        System.out.println("Escibe un numero entero: ");
+        System.out.print("Escibe un numero entero: ");
         Scanner sc = new Scanner (System.in);
         int n1 = sc.nextInt();
 
-        System.out.println("Escribe otro numero entero: ");
+        System.out.print("Escribe otro numero entero: ");
         int n2 = sc.nextInt();
 
-        System.out.println("¿Que tipo de operación quieres?, tus opciones son suma, resta, multiplicación y division (escribe solo el símbolo): ");
-        int respuesta = sc.nextInt();
-
-        int suma = (n1 + n2);
-        int resta = (n1 - n2);
-        int multiplicacion = (n1 * n2);
-        int division = (n1 / n2); 
+        System.out.print("Que tipo de operacion quieres?, tus opciones son suma, resta, multiplicacion y division (escribe solo el simbolo): ");
+        String respuesta = sc.next();
 
         switch (respuesta) {
-            case 1:
-                System.out.printl("Tu suma es de: " + suma);
+            case "+":
+                System.out.println("Tu suma es de: " + (n1 + n2));
                 break;
-            case 2: 
-                System.out.printl("Tu resta es de: " + resta);
+            case "-": 
+                System.out.println("Tu resta es de: " + (n1 - n2));
                 break;
-            case 3:
-                System.out.printl("Tu multiplicacion es de: " + multiplicacion);
+            case "*":
+                System.out.println("Tu multiplicacion es de: " + (n1 * n2));
                 break;
-            case 4:
-                System.out.printl("Tu division es de: " + division);
+            case "/":
+                System.out.println("Tu division es de: " + (n1 / n2));
                 break;
             default:
-                System.out.println("No has colocado bien el simbolo, vuelve a intentarlo");
+                System.out.println("No has colocado bien el símbolo, vuelve a intentarlo");
                 break;
         }
     }
---
+++ Ejer18.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ejer18 {
+    public static void main(String[] args) {
+        
+        System.out.print("Escribe las horas, minutos y segundos  que quieres: ");
+        Scanner sc = new Scanner (System.in);
+        String tiempo = sc.next();
+
+        System.out.println(tiempo + (1));
+
+    }
+    
+}
