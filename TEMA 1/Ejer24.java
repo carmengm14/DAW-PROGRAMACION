b64c34ec305b5a9b62ec030329bc81101c51625b
carmengm14 <alugar4059@ieselcamincas.org>
2021-10-11 11:17:45 +0200

    hola

--- TEMA 1/Ejer24.java
+++ TEMA 1/Ejer24.java
@@ -7,20 +7,21 @@ public class Ejer24 {
         System.out.println("Escribe la nota, para finalizar tienes que pulsar -1: ");
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
+        int contador = 0;
+        double total = 0;
         
-        int diez = 0;
-        int positives = 0;
-        while (num != -1){
-            num = sc.nextInt();         
-        }
-        if (num == 10) {
-            diez = diez +1;
-         }else {
-            System.out.println("No tienes dieces");
-         }
+        do{
+            total = total +1;
+            contador ++;
+        } while (num !=-1);
 
-        System.out.println("Tu nota media es de: " +  + " puntos.");
-        System.out.println("Tienes: " + diez + " dieces." );
+        System.out.print("Tu media es de: " + total/contador + " puntos.");
+
+        if (num == 10) {
+            System.out.println("Hay 1 o más dieces en tus notas");
+        }else{
+            System.out.println("No hay dieces en tus notas");
+        }
         
         sc.close();
     }
