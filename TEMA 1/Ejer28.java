e697855b4e01de66196b99168ece1af48f5b8300
carmengm14 <alugar4059@ieselcamincas.org>
2021-10-13 10:12:15 +0200

    no acabado

---
+++ TEMA 1/Ejer28.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ejer28 {
+    public static void main(String[] args) {
+        System.out.print("Escribe el número que quieres saber si es primo o no: ");
+        Scanner sc = new Scanner(System.in);
+        double numero = sc.nextDouble();
+
+       
+
+    }
+}
