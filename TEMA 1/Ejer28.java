405c642646acbb51f0157988dbefeaca01ab7e0f
carmengm14 <alugar4059@ieselcamincas.org>
2021-10-15 09:17:31 +0200

    ¿¿¿

--- TEMA 1/Ejer28.java
+++ TEMA 1/Ejer28.java
@@ -1,20 +1,37 @@
 import java.util.Scanner;
 
 public class Ejer28 {
-    public static void main(String[] args) {
-        System.out.print("Escribe el número que quieres saber si es primo o no: ");
-        Scanner sc = new Scanner(System.in);
-        int numero = sc.nextInt();
-        int divisor = 2;
+  public static void main(String[] args) {
+    System.out.print("Escribe un numero para saber si es primo: ");
+    Scanner sc = new Scanner(System.in);
+    int numero = sc.nextInt();
+    int divisor = 2;
+    boolean esPrimo = true;
 
-      while (numero % divisor == 0) {
-          System.out.println(numero +"/"+ divisor + " = " +numero + " es primo.");
-          divisor++;
-      }   
-      if (numero % divisor != 0) {
-        System.out.println(numero +"/"+ divisor + " = " +numero + " no es primo.");
+    while ((divisor < numero) && esPrimo) {
+       
+      if (numero % divisor == 0) {
+        esPrimo = false;
       }
-      
-      
+      divisor++;
     }
+
+    if (esPrimo == true){
+      System.out.println("Es primo");
+    }else{
+      System.out.println("No es primo");
+    }
+  }
 }
+  
+  
+ /*   while (divisor < numero && esPrimo) {
+    System.out.println(numero +"/"+ divisor + " = " +numero + " no es primo.");
+    divisor++;        
+  }   
+  if (numero % divisor != 0) {
+
+    System.out.println(numero +"/"+ divisor + " = " +numero + " es primo.");
+  }
+  */
+
