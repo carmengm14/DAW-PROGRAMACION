04d1cbd6c95f862877c20c9ee037bada299e5171
carmengm14 <alugar4049@ieselcaminas.org>
2021-10-16 16:58:54 +0200

    hola

--- TEMA 1/Ejer30.java
+++ TEMA 1/Ejer30.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class Ejer30 {
     public static void main(String[] args) {
-        
+        System.out.print("Dime una palabra para saber si es palíndroma: ");
+        Scanner sc = new Scanner(System.in);
+        String palabra = sc.next();
+        sc.close();
+
+        palabra.length();
     }
 }
