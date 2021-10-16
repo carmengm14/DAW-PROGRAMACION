543451e90e2dfb2f0955575a7ddaf5b2191fc953
carmengm14 <alugar4049@ieselcaminas.org>
2021-10-16 18:05:01 +0200

    hola buenas

--- TEMA 1/Ejer30.java
+++ TEMA 1/Ejer30.java
@@ -2,11 +2,30 @@ import java.util.Scanner;
 
 public class Ejer30 {
     public static void main(String[] args) {
-        System.out.print("Dime una palabra para saber si es palíndroma: ");
+
+        boolean palindromo = true;
+
+        System.out.print("Dime una palabra o frase para saber si es palíndromo: ");
         Scanner sc = new Scanner(System.in);
-        String palabra = sc.next();
+        String palabra = sc.nextLine();
         sc.close();
 
-        palabra.length();
+        int tamaño = palabra.length();
+        int limite = tamaño/2;
+        
+        for (int i = 0; i < limite; i++) {
+            char caracter1 = palabra.charAt(i);
+            char caracter2 = palabra.charAt(tamaño-1-i);
+           
+            if (caracter1 != caracter2){
+                palindromo = false;
+            }
+        }
+        if (palindromo) {
+            System.out.println("Tu palabra: " + palabra + " es palindromo.");
+        } else {
+            System.out.println("Tu palabra: " + palabra + " no es palindromo.");
+        }
+
     }
 }
