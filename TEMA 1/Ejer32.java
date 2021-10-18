374d9b39459d433d1d501076b41f9ffabf8f07d0
carmengm14 <alugar4059@ieselcamincas.org>
2021-10-18 09:49:43 +0200

    casi

---
+++ TEMA 1/Ejer32.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ejer32 {
+    public static void main(String[] args) {
+      
+        int [] orden;
+        orden = new int [11];
+
+        System.out.print("Escribe 10 numeros: ");
+        Scanner sc = new Scanner(System.in);
+        int nums = sc.nextInt();
+        sc.close();
+
+        for (int i = 0; i < orden.length; i++) {
+            
+        }
+
+    }
+}
