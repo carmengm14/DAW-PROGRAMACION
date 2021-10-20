dacd04e6868fc12cfc2b663969be49e06df41c84
carmengm14 <alugar4059@ieselcamincas.org>
2021-10-20 09:43:40 +0200

    impresionante

--- TEMA 1/Ejer32.java
+++ TEMA 1/Ejer32.java
@@ -2,18 +2,21 @@ import java.util.Scanner;
 
 public class Ejer32 {
     public static void main(String[] args) {
-      
-        int [] orden;
-        orden = new int [11];
-
-        System.out.print("Escribe 10 numeros: ");
-        Scanner sc = new Scanner(System.in);
-        int nums = sc.nextInt();
+    
+        int[] nums = new int[10];
+        int i;
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Por favor, introduzca 10 números enteros.");
+        System.out.println("Pulse la tecla INTRO después de introducir cada número.");
         sc.close();
 
-        for (int i = 0; i < orden.length; i++) {
-            
+        for (i = 0; i < 10; i++) {
+          nums[i] = Integer.parseInt(System.console().readLine());
         }
-
+        
+        System.out.println("Los números introducidos, al revés, son los siguientes:");
+        for (i = 9; i >= 0; i--) {
+          System.out.println(nums[i]);
+        }
+      }
     }
-}
---
+++ TEMA 1/Ejer33.java
@@ -0,0 +1,113 @@
+import java.util.Scanner;
+
+public class Ejer33 {
+    public static void main(String[] args) {
+        System.out.print("Escribe los numeros de tu DNI sin la letra: ");
+        Scanner sc = new Scanner(System.in);
+        int dni = sc.nextInt();
+
+        char[] nums;
+        nums = new char[22];
+ 
+        
+        switch (dni % 23) {
+            case 0:
+            nums [0] = 'T';
+            System.out.println(nums);
+                break;
+            case 1:
+            nums [1] = 'R';
+            System.out.println(nums);
+                break;
+            case 2:
+            nums [2] = 'W';
+            System.out.println(nums);
+                break;
+            case 3:
+            nums [3] = 'A';
+            System.out.println(nums);
+                break;
+            case 4:
+            nums [4] = 'G';
+            System.out.println(nums);
+                break;
+            case 5:
+            nums [5] = 'M';
+            System.out.println(nums);
+                break;
+            case 6:
+            nums [6] = 'Y';
+            System.out.println(nums);
+                break;
+            case 7:
+            nums [7] = 'F';
+            System.out.println(nums);
+                break;
+            case 8:
+            nums [8] = 'P';
+            System.out.println(nums);
+                break;
+            case 9:
+            nums [9] = 'D';
+            System.out.println(nums);
+                break;
+            case 10:
+            nums [10] = 'X';
+            System.out.println(nums);
+                break;
+            case 11:
+            nums [11] = 'B';
+            System.out.println(nums);
+                break;
+            case 12:
+            nums [12] = 'N';
+            System.out.println(nums);
+                break;
+            case 13:
+            nums [13] = 'J';
+            System.out.println(nums);
+                break;
+            case 14:
+            nums [14] = 'Z';
+            System.out.println(nums);
+                break;
+            case 15:
+            nums [15] = 'S';
+            System.out.println(nums);
+                break;
+            case 16:
+            nums [16] = 'Q';
+            System.out.println(nums);
+                break;
+            case 17:
+            nums [17] = 'V';
+            System.out.println(nums);
+                break;
+            case 18:
+            nums [18] = 'H';
+            System.out.println(nums);
+                break;
+            case 19:
+            nums [19] = 'L';
+            System.out.println(nums);
+                break;
+            case 20:
+            nums [0] = 'T';
+            System.out.println(nums);
+                break;
+            case 21:
+            nums [21] = 'K';
+            System.out.println(nums);
+                break;
+            case 22:
+            nums [22] = 'E';
+            System.out.println(nums);
+                break;
+            default:
+            System.out.println("NO ES VALIDO");
+                break;
+        }
+
+    }
+    
+}
