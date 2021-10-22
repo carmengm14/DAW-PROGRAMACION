fbc66d900cb6fe730088afdc16ec50d7f75305fe
carmengm14 <alugar4059@ieselcamincas.org>
2021-10-22 10:42:53 +0200

    hola

--- TEMA 1/Ejer34.java
+++ TEMA 1/Ejer34.java
@@ -2,15 +2,41 @@ import java.util.Scanner;
 
 public class Ejer34 {
     public static void main(String[] args) {
-        System.out.print("Escribe los numeros de tu NIE sin la letra: ");
+        
+        System.out.print("Escribe la letra de tu NIE en mayuscula: ");
         Scanner sc = new Scanner(System.in);
+        String letraprincipio = sc.next();
+
+        System.out.print("Escribe el numero de tu nie: ");
         int nie = sc.nextInt();
         sc.close();
         
-        String letras = "XTZ";
-        letras.charAt(nie % 23 + 1);
-        System.out.println(letras);
-
+        
+      if (letraprincipio == "X") {
+        nie= nie;
+      } else if (letraprincipio == "Y") {
+        nie= nie + 10000000;
+          
+      }else if (letraprincipio == "Z") {
+       nie = nie + 20000000;
+      }
+    
+        /*  if (letraprincipio == "X") {
+            letraprincipio = "0";
+        } else if (letraprincipio == "Y") {
+            letraprincipio = "1";
+        }else if (letraprincipio == "z"){
+            letraprincipio = "2";
+        }else{
+            System.out.println("NO ESTA PERMITIDO");
+        }
+        */
+        
+        int letrafinal;
+        char [] letrastotales = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P','D','X','B','N','J','Z','S','Q','V','H','L','T','K','E'};
+        letrafinal = nie % 23;
+        System.out.println(letraprincipio + "" + nie + "" + letrastotales[letrafinal] );
+        
     }
     
 }
