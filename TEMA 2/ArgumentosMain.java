a6f85e9d9a11c404628abf28bd4482f789147287
carmengm14 <alugar4059@ieselcamincas.org>
2021-10-27 08:58:46 +0200

    hola

--- TEMA 2/ArgumentosMain.java
+++ TEMA 2/ArgumentosMain.java
@@ -1,7 +1,8 @@
 public class ArgumentosMain {
    public static void main(String[] args) {
         for (int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
+            System.out.print(args[i]);
+            System.out.println(" " + "Argumentos: " + i);
         }
     }
 }
