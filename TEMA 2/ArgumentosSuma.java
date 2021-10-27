fe38a69eb0f43e68d48cb4905a49649c96c09be4
carmengm14 <alugar4059@ieselcamincas.org>
2021-10-27 09:39:27 +0200

    hola

--- TEMA 2/ArgumentosSuma.java
+++ TEMA 2/ArgumentosSuma.java
@@ -9,5 +9,6 @@ public class ArgumentosSuma {
             suma= suma + temp;
         }
         System.out.println("Suma = " + suma);
-        System.out.println("===============");    }
+        System.out.println("===============");    
+    }
 }
