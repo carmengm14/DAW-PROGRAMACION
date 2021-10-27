24a54c164c1a106d77ce24ce88355882e197dafe
carmengm14 <alugar4059@ieselcamincas.org>
2021-10-27 09:32:13 +0200

    hola

---
+++ TEMA 2/ArgumentosSuma.java
@@ -0,0 +1,13 @@
+public class ArgumentosSuma {
+    public static void main(String[] args) {
+        double suma= 0;
+        System.out.println("===============");
+        System.out.println("Arguentos: " + args.length);
+        System.out.println("---------------");
+        for (int i = 0; i < args.length; i++) { 
+            Double temp= Double.parseDouble(args[i]);
+            suma= suma + temp;
+        }
+        System.out.println("Suma = " + suma);
+        System.out.println("===============");    }
+}
