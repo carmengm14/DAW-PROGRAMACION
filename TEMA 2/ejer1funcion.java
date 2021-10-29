f21faeccb869fe456d807539d71ecd393b4e4a0e
carmengm14 <alugar4059@ieselcamincas.org>
2021-10-29 09:15:49 +0200

    h

--- TEMA 2/ejer1funcion.java
+++ TEMA 2/ejer1funcion.java
@@ -4,13 +4,13 @@ public class ejer1funcion {
     public static void main(String[] args) {
         Scanner sc= new Scanner (System.in);
         System.out.print("Escribe un numero: ");
-        int n1 = sc.nextInt();
+        int numero = sc.nextInt();
         boolean n2 = true;
        
-        while ((n1 > 0) && n2) {
+        while ((numero >= 0) && n2) {
             System.out.print("Escribe otro numero: ");
-            n1 = sc.nextInt();
-            if (n2 = false) {
+            numero = sc.nextInt();
+            if (n2 == false) {
                 System.out.println("No se puede poner numeros no enteros");
             }
         }
