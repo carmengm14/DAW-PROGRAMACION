a51839239781b0c96e76f8a7a875e8286d5ee98a
carmengm14 <alugar4049@ieselcaminas.org>
2021-11-02 19:28:54 +0100

    hola

--- TEMA 2/Entrada.java
+++ TEMA 2/Entrada.java
@@ -1,18 +1,43 @@
 import java.util.Scanner;
 
-public class Entrada { 
-    public static int getInteger(int numero) {
-        Scanner sc= new Scanner (System.in);
-        System.out.print("Escribe un numero: ");
-        numero = sc.nextInt();
-        boolean n2 = true;
-        while ((numero >= 0) && n2) {
-            System.out.print("Escribe otro numero: ");
-            numero = sc.nextInt();
-            if (n2 == false) {
-                System.out.println("No se puede poner numeros no enteros");
+public class Entrada {
+
+    public static int getInteger (){
+        int numero;
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            
+            System.out.print("Introduce un valor: ");
+            
+           if (sc.hasNextInt()){
+                numero = sc.nextInt();
+                break;
+            }else{
+                sc.nextLine();
             }
         }
         sc.close();
+        return numero;
+    }
+
+    public static double getDouble (){
+        double numero;
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            
+            System.out.print("Introduce un valor: ");
+            
+           if (sc.hasNextDouble()){
+                numero = sc.nextDouble();
+                break;
+            }else{
+                sc.nextLine();
+            }
+        }
+        sc.close();
+        return numero;
+    }
+    public static void main (String args[]){
+      getDouble();
     }
 }
