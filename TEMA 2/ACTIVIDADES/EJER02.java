6ec5733e448733fd87410e1b3ef6d50251e6e1bb
carmengm14 <alugar4059@ieselcamincas.org>
2021-11-10 10:35:09 +0100

    buenas

--- TEMA 2/ACTIVIDADES/EJER02.java
+++ TEMA 2/ACTIVIDADES/EJER02.java
@@ -6,6 +6,27 @@ posible formar un triángulo:
 Si cualquiera de las tres longitudes es mayor que la suma de las otras dos, no puedes formar un triángulo
 Escribe un método llamado  esTriangulo  que toma tres enteros como argumentos y devuelve true o
 false, dependiendo de si puede o no formar un triángulo a partir de palitos con las longitudes dadas. */ 
+import java.util.Scanner;
 public class EJER02 {
-    
+    public static boolean esTriangulo (int x, int y, int z){
+        if ( x + y  > z && x + z > y && y + z > x) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.print("Escribe la longitud de un lado: ");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        System.out.print("Escribe la longitud de otro lado: ");
+        int y = sc.nextInt();
+
+        System.out.print("Escribe la longitud de otro lado: ");
+        int z = sc.nextInt();
+        sc.close();
+
+        System.out.println(esTriangulo(x, y, z));
+    }
 }
