ec28f2367183b89b22acdaaa91f74c474387f6bb
carmengm14 <alugar4059@ieselcamincas.org>
2021-11-10 10:18:58 +0100

    hola

---
+++ TEMA 2/ACTIVIDADES/EJER02.java
@@ -0,0 +1,11 @@
+package ACTIVIDADES;
+/*  EsTriangulo.java  Si te dan tres palitos, puedes o no ser capaz de organizarlos en un triángulo. Por
+ejemplo, si uno de los palos mide 12 cm de largo y los otros dos tienen un cm de largo, no podrás hacer que
+los palitos cortos se encuentren en el medio. Para tres longitudes, hay una prueba simple para ver si es
+posible formar un triángulo: 
+Si cualquiera de las tres longitudes es mayor que la suma de las otras dos, no puedes formar un triángulo
+Escribe un método llamado  esTriangulo  que toma tres enteros como argumentos y devuelve true o
+false, dependiendo de si puede o no formar un triángulo a partir de palitos con las longitudes dadas. */ 
+public class EJER02 {
+    
+}
