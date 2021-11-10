8bb54e06e89f8c6425e323ffdf610a54591161af
carmengm14 <alugar4059@ieselcamincas.org>
2021-11-10 10:41:08 +0100

    hola

---
+++ TEMA 2/ACTIVIDADES/EJER03.java
@@ -0,0 +1,23 @@
+package ACTIVIDADES;
+/* Potencia.java  Escribe un método recursivo llamado  potencia  que toma un doble x y un entero n y
+devuelve x^n.  */
+import java.util.Scanner;
+
+public class EJER03 {
+    public static double potencia(double x, int n){
+        double potencia = Math.pow(x, n);
+        return potencia;
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Escribe un numero: ");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        
+        System.out.print("Escribe la potencia: ");
+        int n = sc.nextInt();
+        sc.close();
+
+        System.out.println(potencia(x, n));
+    }
+}
