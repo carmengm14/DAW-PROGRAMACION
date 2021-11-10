bba5913ccfeac9aae070b585e009548d0b388ab1
carmengm14 <alugar4059@ieselcamincas.org>
2021-11-10 10:11:52 +0100

    hola

---
+++ TEMA 2/ACTIVIDADES/EJER01.java
@@ -0,0 +1,29 @@
+package ACTIVIDADES;
+/* EsDivisible.java  Escribe un método llamado  esDivisible  que toma dos enteros, n y m, y devuelve
+verdadero si n es divisible por m, y falso en caso contrario. */
+import java.util.Scanner;
+public class EJER01 {
+    
+    public static boolean esDivisible(int n, int m){
+        if (n % m == 0 ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.print("Escribe un numero: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.print("Escribe otro numero: ");
+        int m = sc.nextInt();
+        sc.close();
+        
+        if (esDivisible(n, m)) {
+            System.out.println(n + "/" + m + " es divisible");
+        }else{
+            System.out.println(n + "/" + m + " no es divisible");
+        }
+    }
+}
