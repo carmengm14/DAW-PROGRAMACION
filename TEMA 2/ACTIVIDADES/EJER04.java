4aa08cfa0ee37329aff48f7674595ac3fc0165dd
carmengm14 <alugar4059@ieselcamincas.org>
2021-11-12 09:46:39 +0100

    buena

--- TEMA 2/ACTIVIDADES/EJER04.java
+++ TEMA 2/ACTIVIDADES/EJER04.java
@@ -1,16 +1,20 @@
 package ACTIVIDADES;
-
 /* Positivos.java  Escribe un método llamado  positivos  que muestre cuántos números de un array son
 positivos.  */
-import java.util.Scanner;
 public class EJER04 {
+    public static int positivos (){
+        int positivos = 0;
+        int [] notas = {5, 10, -4, 3, 5, 7, 8, -5}; 
 
-    public static boolean positivos(int[] x) {
+        for (int i = 0; i < notas.length; i++) {
+            if (notas[i] >= 0) {
+                positivos++;
+            }
+        }
+        return positivos;
     }
+
     public static void main(String[] args) {
-        System.out.println("Escribe un numero entero negativo o positivo :");
-        Scanner sc = new Scanner(System.in);
-        sc.close();
-        int[] x = new int[];
+        System.out.println(positivos());
     }
 }
