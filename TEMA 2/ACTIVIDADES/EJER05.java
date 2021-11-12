788f3236583bd1e544e4c71c37370724d580641b
carmengm14 <alugar4059@ieselcamincas.org>
2021-11-12 10:42:15 +0100

    hola

---
+++ TEMA 2/ACTIVIDADES/EJER05.java
@@ -0,0 +1,13 @@
+package ACTIVIDADES;
+/* LetraDni.java  Realiza un método llamado  letraDni  que devuelva la letra correspondiente a un DNI
+sin letra */
+public class EJER05 {
+    public static char letraDNI(int dni) {
+        char[] nums;
+        nums = new char[22];
+        
+    }
+    public static void main(String[] args) {
+        System.out.println(letraDNI(20456595));
+    }
+}
