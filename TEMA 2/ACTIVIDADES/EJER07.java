021c152e63c3bffc6b874c75266eb272ed74e6be
carmengm14 <alugar4049@ieselcaminas.org>
2021-11-12 19:23:14 +0100

    buenas

---
+++ TEMA 2/ACTIVIDADES/EJER07.java
@@ -0,0 +1,13 @@
+
+/* Primos.java Crea un programa que muestre en pantalla todos los números primos entre 3 y 100,
+usando la función creada en el ejercicio anterior. Puedes llamar a la función creada en el ejercicio anterior
+de la siguiente forma: Primo.esPrimo(numero);
+*/
+public class EJER07 {
+    public static void main(String[] args) {
+        for (int i = 3; i < 101; i++) {
+            System.out.println(EJER06.esPrimo(i));
+        }
+        
+    }
+}
---
+++ TEMA 2/ACTIVIDADES/Mayor.java
@@ -0,0 +1,19 @@
+/*  Mayor.java Realiza un programa que pida la edad al usuario y muestre un mensaje si es mayor de edad */
+import java.util.Scanner;
+public class Mayor {
+    public static void MayorDeEdad(int edad) {
+        if (edad >= 18) {
+            System.out.println("Eres mayor de edad porque tienes: " + edad + " años");
+        } else {
+            System.out.println("Eres menor de edad porque tienes: " + edad + " años");
+        }
+
+    }
+    public static void main(String[] args) {
+       System.out.print("Dime tu edad: "); 
+       Scanner sc = new Scanner(System.in);
+       int edad = sc.nextInt();
+       sc.close();
+       MayorDeEdad(edad);
+    }
+}
