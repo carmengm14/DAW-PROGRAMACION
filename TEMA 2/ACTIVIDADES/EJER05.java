0d798520308d9ff70a9fee6b357003e05a93a9f6
carmengm14 <alugar4059@ieselcamincas.org>
2021-11-12 11:51:07 +0100

    buenas

--- TEMA 2/ACTIVIDADES/EJER05.java
+++ TEMA 2/ACTIVIDADES/EJER05.java
@@ -1,13 +1,21 @@
 package ACTIVIDADES;
+
+import java.util.Scanner;
+
 /* LetraDni.java  Realiza un método llamado  letraDni  que devuelva la letra correspondiente a un DNI
 sin letra */
 public class EJER05 {
-    public static char letraDNI(int dni) {
-        char[] nums;
-        nums = new char[22];
-        
+    public static void letraDNI(int dni){
+        char[] letras = {'T', 'R', 'W', 'A', 'G', 'M', 'Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
+        int operacion = dni % 23;
+        System.out.println("Tu DNI completo es: " + dni + letras[operacion]);
     }
     public static void main(String[] args) {
-        System.out.println(letraDNI(20456595));
+        System.out.print("Introduce el numero de tu DNI sin letra: ");
+        Scanner sc = new Scanner(System.in);
+        int dni = sc.nextInt(); 
+        letraDNI(dni);
+        sc.close();
+
     }
 }
