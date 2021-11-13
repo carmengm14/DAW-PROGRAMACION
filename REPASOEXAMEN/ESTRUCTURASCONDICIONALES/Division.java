a8c6fd0d14bea14cea51b74174155ada79106fb3
carmengm14 <alugar4049@ieselcaminas.org>
2021-11-13 14:05:59 +0100

    hola

---
+++ REPASOEXAMEN/ESTRUCTURASCONDICIONALES/Division.java
@@ -0,0 +1,28 @@
+package ESTRUCTURASCONDICIONALES;
+import java.util.Scanner;
+/* Escriba un programa que pida dos números enteros y que calcule la división, indicando si la división es exacta o no. */
+
+public class Division {
+    public static void main(String[] args) {
+        System.out.print("Dividendo: ");
+        Scanner sc = new Scanner(System.in);
+        int dividendo = sc.nextInt();
+
+        System.out.print("Divisor: ");
+        int divisor = sc.nextInt();
+        sc.close();
+        
+        if (dividendo % divisor == 0) {
+            System.out.println("La división es exacta.");
+        }else{
+            System.out.println("La división no es exacta.");
+        }
+
+        int cociente = dividendo / divisor ;
+        int resto = dividendo % divisor ;
+
+        System.out.println("Cociente: " + cociente);
+        System.out.println("Resto: " + resto);
+
+    }
+}
---
+++ REPASOEXAMEN/ESTRUCTURASCONDICIONALES/PalabraMasLarga.java
@@ -0,0 +1,28 @@
+package ESTRUCTURASCONDICIONALES;
+import java.time.Year;
+import java.util.Scanner;
+/* Escriba un programa que pida al usuario dos palabras, y que indique cuál de ellas es la más larga y por cuántas letras lo es. */
+
+public class PalabraMasLarga {
+    public static void main(String[] args) {
+        System.out.print("Palabra 1: ");
+        Scanner sc = new Scanner(System.in);
+        String palabra1 = sc.nextLine();
+
+        System.out.print("Palabra 2: ");
+        String palabra2 = sc.nextLine();
+
+        sc.close();
+        int conteo1 = palabra1.length(); //esto cuenta numero de letras
+        int conteo2 = palabra2.length();
+
+        if (conteo1 > conteo2){
+            System.out.printf(" La palabra " + palabra1 + " tiene " + (conteo1-conteo2) + " letras mas que " + palabra2);
+          }if (conteo1 < conteo2){
+            System.out.printf(" La palabra " + palabra2 + " tiene " + (conteo2-conteo1) + " letras mas que " + palabra1);
+          }if(conteo1 == conteo2){
+            System.out.printf( "Las dos palabras tienen el mismo largo");
+          }
+    }
+        
+}
