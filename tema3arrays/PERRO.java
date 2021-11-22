3ef92517bdbdcb62a0baf603be738032558522b8
carmengm14 <alugar4059@ieselcamincas.org>
2021-11-22 09:53:20 +0100

    hola

--- tema3arrays/PERRO.java
+++ tema3arrays/PERRO.java
@@ -6,11 +6,27 @@ public class PERRO{
     String color;
     String nombre;
     boolean tieneChip;
-    
+    static void ladrar(){
+        System.out.println("Guau guau...");
+    }
+
     public static void main(String[] args) {
         System.out.println("Inicio del programa");
         PERRO laika = new PERRO();
         PERRO roque = new PERRO();
+
+        laika.nombre = "Laika";
+        laika.edad = 10;
+
+        roque.nombre = "Roque";
+        roque.color = "Negro";
+
+        System.out.println(laika.nombre);
+        System.out.println(laika.edad);
+        PERRO.ladrar();
+        System.out.println(roque.nombre);
+        System.out.println(roque.color);
+        
         System.out.println("Fin del programa.");
     }
 }
