2de31062ae0921cbbe85b139c922c9086da650e9
carmengm14 <alugar4059@ieselcamincas.org>
2021-11-26 09:57:56 +0100

    hola

--- tema3arrays/pruebas/EJEMPLOARRAY.java
+++ tema3arrays/pruebas/EJEMPLOARRAY.java
@@ -1,8 +1,9 @@
 //EJEMPLO DE RELLENAR ARRAY DE JULIO
 public class EJEMPLOARRAY {
     public static void main(String[] args) {
-        //Creamos un array de 10 elementos que por ahora estan vacios con 0.
-        int [] externo = new int [10]; 
+        // matriz de 4 * 3      
+        int [][] matrizexterno = new int [4][3]; 
+        //Creamos un array de 100 elementos que por ahora estan vacios con 0.
         int [] externoGordo = new int [100];
 
         //Escribimos en el metodo llamado, nuestro array y el calor minimo y el maximo que queremos.
@@ -11,7 +12,7 @@ public class EJEMPLOARRAY {
         public static void rellenaArray(int[] array, int min, int max){
             
             AQUI SE USA ARRAY.LENGTH EN VEZ DE PONER UN NUMERO YA QUE SI PUSIESEMOS UN NUMERO COMO POR 
-            EJEMPLO 10 Y EL USUARIO PONE 20 YA NO RECORRERIA TODO, SIN EMBARGO CON EL ARRAY.LENGHT RECORRERÁ
+            EJEMPLO 10 Y EL USUARIO PONE 20 YA NO RECORRERIA LOS NUMEROS, ENTONCES CON EL .LENGHT
             EL NUMERO QUE PONGA EL USUARIO
 
 		    for (int i = 0; i < array.length; i++) {
@@ -25,8 +26,12 @@ public class EJEMPLOARRAY {
 	    }
         */
         //Esta funcion no devuelve nada porque esta hecha con void.
-       Utilidades.rellenaArray(externo, 20, 40); 
-       Utilidades.rellenaArray(externoGordo, 50, 70); 
 
+        //matriz de dos 
+       Utilidades.rellenaMatriz(matrizexterno, 20, 40); 
+       //mostramos la matriz
+       Utilidades.mostrarMatriz(matrizexterno);
+       //matriz de una columna
+       Utilidades.rellenaArray(externoGordo, 50, 70); 
     }
 }
