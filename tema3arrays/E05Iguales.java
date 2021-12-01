f8b37a6b70eeafe2dfcb954c60fcb828fc53e7eb
carmengm14 <alugar4059@ieselcamincas.org>
2021-12-01 09:34:19 +0100

    hola

--- tema3arrays/E05Iguales.java
+++ tema3arrays/E05Iguales.java
@@ -4,18 +4,26 @@ public class E05Iguales {
     public static void main(String[] args) {
         int [] array1 = {1,5,6,8,9};
         int [] arrayClonado = array1.clone();
-        //array1.clone();
+    
         boolean iguales = true;
                 
-        System.out.println("SON IGUALES LOS SIGUIENTES NUMEROS: ");
-        for (int i = 0; i < array1.length; i++) {
-            System.out.print(array1[i] + " = ");
-                    if(array1[i] == arrayClonado[i]){
-                        System.out.println(iguales);
-                    }else{
-                        System.out.println(iguales = false);
-                    }
-        }   
+        System.out.print("LOS NUMEROS SON: ");
+        if (array1.length != arrayClonado.length) {
+            System.out.println( "DIFERENTES");
+        }else{
+            for (int i = 0; i < array1.length; i++) {
+                if (array1[i] != arrayClonado[i]) {
+                    iguales = false;
+                    break;
+                }
+            }
+            if(iguales){
+                System.out.println( "IGUALES");
+            }else{
+                System.out.println( "DIFERENTES");
+            }
+        }
+        
         System.out.println("FIN");        
     }
     
---
+++ tema3arrays/E06ParesImpares.java
@@ -0,0 +1,4 @@
+/* Escribid un programa Java para encontrar el número de enteros pares e impares en un array dado de números enteros. */
+public class E06ParesImpares {
+    
+}
