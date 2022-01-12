57adb8cd83260f61feda1c926c6f080979ac73c8
carmengm14 <alugar4059@ieselcamincas.org>
2022-01-12 08:46:38 +0100

    hola

--- tema3/objetos/Pajaro.java
+++ tema3/objetos/Pajaro.java
@@ -41,8 +41,8 @@ class Pajaro {
    
    class Crear {
     public static void main(String[] args) { // metodo main
-     pajaro p;
-     p = new pajaro();
+     Pajaro p;
+     p = new Pajaro();
      p.setEdad(5);
      p.printEdad();
     }
