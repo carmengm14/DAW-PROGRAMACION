a82b02f5e0a455b729848c157d44734627aaa5b7
carmengm14 <alugar4059@ieselcamincas.org>
2022-01-12 15:35:01 +0100

    hola

---
+++ tema3/strings/E18Pasatiempos.java
@@ -0,0 +1,14 @@
+/** Hay un tipo de pasatiempos que propone descifrar un texto del que se han suprimido las vocales. Por ejemplo, el texto ".n .j.mpl. d. p.s.t..mp.s" se descrifra sustituyendo cada punto por una vocal. La solución es "un ejemplo de pasatiempos".
+Diseña un programa que ayude al creador de pasatiempos. Recibirá una cadena y mostrará otra en la que cada vocal ha sido reemplazada por un punto. */
+
+public class E18Pasatiempos {
+    public static void cambiarVocales(String cadena) {
+        
+        
+    }
+    public static void main(String[] args) {
+
+        String cadena = Utilidades.leerCadena("Escribe tu cadena: ");
+        cambiarVocales(cadena);
+    }
+}
