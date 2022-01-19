c30c76854f14d12e88142209a2976bc71f8c225a
carmengm14 <alugar4059@ieselcamincas.org>
2022-01-19 10:41:49 +0100

    hola

--- tema3/strings/E18Pasatiempos.java
+++ tema3/strings/E18Pasatiempos.java
@@ -3,8 +3,14 @@ Diseña un programa que ayude al creador de pasatiempos. Recibirá una cadena y 
 
 public class E18Pasatiempos {
     public static void cambiarVocales(String cadena) {
+        //hacer las letras todas minusculas para compararlas bien.
+        cadena.toLowerCase();
         
-        
+        for (int i = 0; i < cadena.length(); i++) {
+            if (cadena.charAt(i) == 'a' || cadena.charAt(i) == 'e' || cadena.charAt(i) == 'i' || cadena.charAt(i) == 'o' || cadena.charAt(i) == 'u') {
+               
+            }
+        }
     }
     public static void main(String[] args) {
 
