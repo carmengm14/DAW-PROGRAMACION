1ea10fc7c89de1b52c02456da69bb99071101fd9
carmengm14 <alugar4059@ieselcamincas.org>
2022-01-19 10:44:14 +0100

    hola

--- tema3/strings/E18Pasatiempos.java
+++ tema3/strings/E18Pasatiempos.java
@@ -8,6 +8,7 @@ public class E18Pasatiempos {
         
         for (int i = 0; i < cadena.length(); i++) {
             if (cadena.charAt(i) == 'a' || cadena.charAt(i) == 'e' || cadena.charAt(i) == 'i' || cadena.charAt(i) == 'o' || cadena.charAt(i) == 'u') {
+               char punto = '.';
                
             }
         }
