3a24809cd5aa6e088bf8a703c11ecc83644e8cf0
carmengm14 <alugar4059@ieselcamincas.org>
2022-02-09 08:50:06 +0100

    hola

---
+++ tema5/TestMusicOrganizer.java
@@ -0,0 +1,12 @@
+public class TestMusicOrganizer {
+    public static void main(String[] args) {
+        MusicOrganizer index1 = new MusicOrganizer();
+        index1.addFile("Cancion 1");
+        index1.addFile("Cancion 2");
+        index1.addFile("Cancion 3");
+        index1.addFile("Cancion 4");
+
+        index1.listFile(4);
+        index1.checkIndex(5);
+    }
+}
