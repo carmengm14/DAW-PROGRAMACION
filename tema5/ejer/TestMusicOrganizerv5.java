ecce929ff57b5b31b6c287727b51265c8c70da23
carmengm14 <alugar4059@ieselcamincas.org>
2022-02-16 08:07:39 +0100

    hola

---
+++ tema5/ejer/TestMusicOrganizerv5.java
@@ -0,0 +1,7 @@
+public class TestMusicOrganizerv5 {
+    public static void main(String[] args) {
+        Track cancion1 = new Track("Maluma", "4 babis", "album1");
+        cancion1.setAlbum("Maria deL Mar");
+        
+    }
+}
