880de2585dbd355e4a93962a9f12248dea3ee561
carmengm14 <alugar4059@ieselcamincas.org>
2022-02-23 09:07:21 +0100

    hola

--- tema5/practica/Usuario.java
+++ tema5/practica/Usuario.java
@@ -1,14 +1,18 @@
+import java.util.ArrayList;
+
 public class Usuario {
     //atributos
     private int id;
     private String email;
     private String password;
+    ArrayList<Usuario> UsuariosNuevos = new ArrayList<Usuario>();
 
     //constructor
     public Usuario(int id, String email, String password){
         this.id = id;
         this.email = email;
         this.password = password;
+        UsuariosNuevos.add(this);
     }
     //getters
     public int getId() {
