fc670b09fec65bcd8250e7355ddb794d143eb644
carmengm14 <alugar4059@ieselcamincas.org>
2022-02-28 08:33:52 +0100

    hola

---
+++ tema5/practica/ParserXML.java
@@ -0,0 +1,14 @@
+/** Crearemos una interfaz llamada ParserXML que tenga un metodo llamado generateXML(), este metodo lo que hara sera devolver por consola el xml correspondiente a los objetos de cada clase, ej: la clase usuario tiene id... y  media tiene UsuarioId */
+
+//interfaz
+interface ParserXML {
+
+}
+//Usuario implementa el ParserXML a la interfaz
+class Usuario implements ParserXML{
+    public void generateXML(){
+        //imprimimos los datos del user
+        
+    }
+}
+
