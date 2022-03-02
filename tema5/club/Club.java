f29a2f6f21f31b360f06d1260bd54fe14bd3d9eb
carmengm14 <alugar4059@ieselcamincas.org>
2022-03-02 11:31:41 +0100

    hola

--- tema5/club/Club.java
+++ tema5/club/Club.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Store details of club memberships.
@@ -9,7 +10,8 @@ import java.util.ArrayList;
 public class Club
 {
     // Define any necessary fields here ...
-    public static ArrayList<String> members = new ArrayList<String>();
+    //como queremos almacenar miembros los guardamos como Membership en vez de String o cualquier otro
+    public static ArrayList<Membership> miembros = new ArrayList<Membership>();
 
     /**
      * Constructor for objects of class Club
@@ -20,12 +22,51 @@ public class Club
         
     }
 
+    /** 
+     Determinar el número de miembros que se han unido en el mes indicado.
+     @param month El mes que nos interesa
+     @return El numero de miembros que se han unido ese mes
+    */
+    public int joinedInMonth(int month){
+        int contador = 0;
+     if (month <= 0 || month > 12) {
+         System.out.println("Esta fuera de rango");
+         return contador;
+     }else{
+        for (Membership miembro : miembros) {
+            if (miembro.getMonth() == month) {
+                contador++;
+            }
+        }
+     }
+     return contador;
+    }
+
+    /** 
+     Metodo que elimina la coleccion a todos los miembros que se unieron en determinado mes.
+     @param month El mes que nos interesa
+     @return El numero de miembros eliminaremos
+    */
+    public int purge(int month){
+        int contador = 0;
+        Iterator<Membership> it = miembros.iterator();
+        while (it.hasNext()) {
+            Membership item = it.next();
+            if (item.getMonth() == month) {
+                it.remove();
+                contador++;
+            }
+        }
+        return contador;
+    }
+
     /**
      * Add a new member to the club's list of members.
      * @param member The member object to be added.
      */
     public void join(Membership member)
     {
+        miembros.add(member);
     }
 
     /**
@@ -34,6 +75,12 @@ public class Club
      */
     public int numberOfMembers()
     {
-        return 0;
+        return miembros.size();   
     }
+
+    //getters and setters
+    public static ArrayList<Membership> getMiembros() {
+        return miembros;
+    }
+    
 }
