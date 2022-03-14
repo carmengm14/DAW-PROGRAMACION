1d81302a9827885a997ffdbc2b1b175c50a11cb5
carmengm14 <alugar4059@ieselcamincas.org>
2022-03-14 10:15:17 +0100

    hola

---
+++ tema5/formas/Cuadrado.java
@@ -0,0 +1,12 @@
+public class Cuadrado extends Rectangulo{
+
+    public Cuadrado(String color, int x, int y, String nombre, double lmayor, double lmenor) {
+        super(color, x, y, nombre, lmayor, lmenor);
+    }
+
+    public void imprimir(){
+        super.imprimir();
+        
+    }
+    
+}
