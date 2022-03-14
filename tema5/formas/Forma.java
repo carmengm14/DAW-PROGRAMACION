5fcb00f0e19b5cf564f3cd214b8388eed41aa367
carmengm14 <alugar4059@ieselcamincas.org>
2022-03-14 08:27:12 +0100

    Hola

--- tema5/formas/Forma.java
+++ tema5/formas/Forma.java
@@ -1,8 +1,33 @@
-import javax.sound.sampled.Port;
+import java.awt.Point;
 
 public class Forma {
     private String color;
-    private Port centro;
+    private Point centro;
     private String nombre;
 
+    public Forma(String color,int x, int y, String nombre) {
+        this.color = color;
+        this.centro = new Point(x,y);
+        this.nombre = nombre;
+    }
+    
+    public void Imprimir(){
+        System.out.println("Color: " + color );
+        System.out.printf("Centro: (%d, %d)\n " + centro.x, centro.y);
+        System.out.println("Nombre: " + nombre);
+    }
+
+    //getters
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void mover(int x, int y){
+        this.centro.x = x;
+        this.centro.y = y;
+    }
 }
