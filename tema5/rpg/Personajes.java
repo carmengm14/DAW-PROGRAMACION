390838a395c63bdb4ffb1fce6a6d9b0047fa3df5
carmengm14 <alugar4059@ieselcamincas.org>
2022-03-18 09:08:29 +0100

    Stat

---
+++ tema5/rpg/Personajes.java
@@ -0,0 +1,5 @@
+public class Personajes {
+    private String nombre;
+    private String raza;
+    private String profesion;
+}
---
+++ tema5/rpg/src/Character/Stat/Stat.java
@@ -0,0 +1,33 @@
+package src.Character.Stat;
+
+public class Stat {
+    //ATRIBUTOS
+    private int value;
+    //CONSTRUCTOR
+    public Stat(int value) {
+        this.value = value;
+    }
+    //METODOS
+
+    //Devuelve el valor actual de la característica
+    public int getValue() {
+        return value;
+    }
+    
+    //Aumenta el valor de la caracteristica en 1
+    public void increase() {
+        this.value = value++;
+    }
+
+    //Decrementa el valor de la caracteristica en 1
+    public void decrease() {
+        this.value = value--;
+    }
+
+    @Override
+    //Devuelve el nombre simple de la clase
+    public String toString(){
+        return getClass().getSimpleName();
+    }
+
+}
