7451b1c596389449e5d49b1423dfeb7693cfba27
carmengm14 <alugar4059@ieselcamincas.org>
2022-04-01 10:32:00 +0200

    hola

--- ficheros/ejemplo2.java
+++ ficheros/ejemplo2.java
@@ -1,11 +1,9 @@
 import java.io.FileInputStream;
-//import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class ejemplo2 {
     public static void main(String[] args) throws IOException {
         String nomFich = "fichero.txt";
-       // FileOutputStream f_out = new FileOutputStream("/home/alumno/Documentos/f/" + nomFich);
         FileInputStream f_in = new FileInputStream("/home/alumno/Documentos/f/" + nomFich);
         int c = f_in.read();
         while (c != -1) {
