8ea15e6055202b9d0f4935c8ce7cb3456258558d
carmengm14 <alugar4059@ieselcamincas.org>
2022-04-04 09:26:50 +0200

    hola

--- ficheros/Ejemplo9.java
+++ ficheros/Ejemplo9.java
@@ -1,3 +1,21 @@
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Ejemplo9 {
-    
+    public static void main(String[] args) throws IOException{
+
+        String nomFich = "f8.txt";
+        FileReader f_in = new FileReader("/home/alumno/Documentos/f/" + nomFich);        
+        char[] buffer = new char[30];
+        int n = f_in.read(buffer);
+        
+        while (n != -1) {
+            for (int i = 0; i < n; i++) {
+                System.out.print(buffer[i]);    
+            }
+            System.out.println(" ");
+            n = f_in.read(buffer);
+        }
+        f_in.close();
+    }
 }
