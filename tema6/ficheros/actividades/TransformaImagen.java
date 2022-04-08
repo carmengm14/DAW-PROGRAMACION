3dc7089f3e12c049a2701f5dde9d08498fd8bb52
carmengm14 <alugar4059@ieselcamincas.org>
2022-04-08 09:17:58 +0200

    hola

--- tema6/ficheros/actividades/TransformaImagen.java
+++ tema6/ficheros/actividades/TransformaImagen.java
@@ -15,23 +15,31 @@ public class TransformaImagen {
                 
         }
         else {
+            //Guardamos la ubicacion del fichero en la variable f para asignarla.
             f = fEnt;
         }
     }
     //METODOS
     public void transformaNegativo() throws IOException {
+        //Leemos el fichero que hemos guardado en f
         FileInputStream reader = new FileInputStream(this.f);
+        //Asignamos una ubicacion al nuevo fichero y escribimos en el el con el FileOutputStream
         File f_Out = new File("./tema6/ficheros/fotos/ficheroFotoNeg.bmp");
         FileOutputStream writer = new FileOutputStream(f_Out);
 
+        //Creamos un array de bytes de tamaño 54
         byte[] cabecera = new byte[54];
-        
+        //Creamos una variable que empieza en 0
         int c = 0;
-
+        //Leemos el fichero que le hemos pasado la ruta previamente (f) en 54 bits
         reader.read(cabecera);
+        //Escribimos el fichero que le hemos pasado la ruta previamente (ficheroFotoNeg.bmp) en 54 bits
         writer.write(cabecera);
 
+        //c lee el fichero f entero
         c = reader.read();
+        //mientras f no sea -1 ( es cuando acaba ), se escribe en el fichero de salida (ficheroFotoNeg.bmp) , ahora para transformar la imagen en 
+        //efecto negativo es 255 - el total de bytes osea la imagen, y es asi como se hará.
         while(c != -1) {
             writer.write(((char)(255 - c)));
             c = reader.read();
