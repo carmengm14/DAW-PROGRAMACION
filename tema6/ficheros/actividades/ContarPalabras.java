2cc41a17498350009542f5174e3e7a58266f14e8
carmengm14 <alugar4059@ieselcamincas.org>
2022-04-11 09:21:26 +0200

    hola

---
+++ tema6/ficheros/actividades/ContarPalabras.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+/*
+Realiza un programa que diga el número de veces que se repite una palabra determinada en un fichero. Tanto el nombre 
+del fichero como la palabra se deben pasar como argumentos en la línea de comandos.
+*/
+public class ContarPalabras {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Escribe el nombre del fichero: ");
+        String fichero = sc.nextLine();
+
+        System.out.print("Escribe la palabra: ");
+        String palabra = sc.nextLine();
+        
+       BufferedReader reader = new BufferedReader(new FileReader("./tema6/ficheros/actividades/" + fichero));
+       String cadena = reader.readLine();
+
+       String[] cadenaSeparada = cadena.toLowerCase().split(" ");
+
+       int contador = 0;
+
+       for (int i = 0; i < cadenaSeparada.length; i++) {
+        if (cadenaSeparada[i].equals(palabra.toLowerCase())) {
+            contador++;
+        }
+        reader.readLine();         
+       }
+        System.out.println("===================");
+        System.out.println("Contenido del fichero: \n" + cadena);
+        System.out.println("===================");
+        System.out.println("En el fichero: " + fichero + "\n" + "La palabra: " + palabra + ", se encuentra: " + contador + " veces.");
+       
+        reader.close();
+        sc.close();
+    }
+ 
+}
