511e2039cad35091a92b082a3e94374acb185f46
carmengm14 <alugar4059@ieselcamincas.org>
2022-04-25 09:30:44 +0200

    HOLA

--- tema6/ficheros/actividades/LeerPrimos.java
+++ tema6/ficheros/actividades/LeerPrimos.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 public class LeerPrimos {
     public static void main(String[] args) throws IOException{
         FileInputStream reader = new FileInputStream("./tema6/ficheros/actividades/primos.txt");
-        int c = reader.read();
-        while (c != -1) {
-            System.out.print((char) c);
-            c = reader.read();
+        int cadena = reader.read();
+        while (cadena != -1) {
+            System.out.print((char) cadena);
+            cadena = reader.read();
         }
         System.out.println("");
         reader.close();
