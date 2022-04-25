bdfeb530b11fb7fedbf688d732a435ec842a6292
carmengm14 <alugar4059@ieselcamincas.org>
2022-04-25 09:59:20 +0200

    hola

--- tema6/ficheros/actividades/EliminarComent.java
+++ tema6/ficheros/actividades/EliminarComent.java
@@ -1,19 +1,37 @@
 /*Realiza un programa que elimine los comentarios de un programa escrito
 en Java. Solo los comentarios que empiezan por //*/
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class EliminarComent {
     public static void main(String[] args) throws IOException {
         //atributos
-    FileReader fileReader1;
-    BufferedReader lectorFich1;
+    FileReader fileReader;
+    BufferedReader lectorFich;
+    BufferedWriter salidaFich;
 
-    fileReader1 = new FileReader("./tema6/ficheros/ff/f2.txt" );
-
-    lectorFich1 = new BufferedReader(fileReader1);
-
+    fileReader = new FileReader("./tema6/ficheros/actividades/eliminarcomentariocodigo.java");
+    lectorFich = new BufferedReader(fileReader);
+    salidaFich = new BufferedWriter(new FileWriter("./tema6/ficheros/actividades/eliminarcomentariocodigo.java"));
+    
+    String cadena = lectorFich.readLine();
+       
+    while (cadena != null) {
+        if (cadena.startsWith("//")) {
+            salidaFich.write("");
+            cadena = lectorFich.readLine();
+        }
+        salidaFich.write(cadena);
+        cadena = lectorFich.readLine();
     }
+    
+    
+fileReader.close();
+lectorFich.close();
+salidaFich.close();
+}
     
 }
---
+++ tema6/ficheros/actividades/estidisticasTexto.java
@@ -0,0 +1,7 @@
+/** Crea un programa que analice un fichero de texto plano y que muestre las siguientes estadisticas:
+ *  nº de letras, nº de palabras, nº de lineas, % de vocales, % de consonantes, % espacios y % de tabuladores.
+*/
+
+public class estidisticasTexto {
+    
+}
