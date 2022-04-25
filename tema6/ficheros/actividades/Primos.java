c81781762ace3c9f00f7547871cccdafd0964a19
carmengm14 <alugar4059@ieselcamincas.org>
2022-04-25 09:25:25 +0200

    hola

--- tema6/ficheros/actividades/Primos.java
+++ tema6/ficheros/actividades/Primos.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 //Escribe un programa que guarde en un fichero con nombre "primos.dat" los números primos que hay entre 1 y 500.
 public class Primos {
     public static void main(String[] args) throws IOException {
-        File ficheroSalida = new File("./tema6/ficheros/actividades/primos.txt");
+        File ficheroSalida = new File("./tema6/ficheros/actividades/primos.dat");
         FileWriter writer = new FileWriter(ficheroSalida);
      
         for (int i = 3; i <= 500; i++) {
