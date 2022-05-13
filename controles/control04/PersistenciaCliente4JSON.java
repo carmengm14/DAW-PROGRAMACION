35543a6ab8b3675441a6fd9f4f7ec20f69fafc18
carmengm14 <alugar4059@ieselcamincas.org>
2022-05-13 09:58:09 +0200

    hola

--- controles/control04/PersistenciaCliente4JSON.java
+++ controles/control04/PersistenciaCliente4JSON.java
@@ -13,6 +13,8 @@ import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
+import com.google.gson.*;
+
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
