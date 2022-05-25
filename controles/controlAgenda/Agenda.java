5cf60279a0f15177e4a87ef8742fd9eb7b3b3cef
carmengm14 <alugar4059@ieselcamincas.org>
2022-05-25 09:09:55 +0200

    cambiado

--- controles/controlAgenda/Agenda.java
+++ controles/controlAgenda/Agenda.java
@@ -13,43 +13,20 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.Scanner;
 
-public class Agenda implements Serializable {
-    //Atributos
-    String nombre, apellidos, email;
-    int telefono;
-
-    //Constructor user pedido por terminal
-        public Agenda(String nombre, String apellidos, String email, int telefono) {
-            this.nombre = nombre;
-            this.apellidos = apellidos;
-            this.email = email;
-            this.telefono = telefono;
-        }
-    //CONSTRUCTOR MANUAL
-        public Agenda() {
-            this.nombre = "CambiarLuego";
-            this.apellidos = "CambiarLuego";
-            this.email = "CambiarLuego";
-            this.telefono = 000000000;
-        }
+public class Agenda implements Serializable{
     
         //METODOS
-        public String toString(){
-            String linea = " ";
-            linea += "Nombre = " + getNombre() + "\t\t" + "Apellidos = " + getApellidos() + "\t" + "Email = " + getEmail() + "\t"+ "Telefono = " + getTelefono() + "." + "\n";
-            return linea;
-        }
         
-        public void escribirFichero() throws IOException{
+        public void escribirFichero(Usuario usuario) throws IOException{
             BufferedWriter bw = null;
             File file = new File("Agenda.dat");
             FileWriter fw = new FileWriter(file.getAbsoluteFile(),true);
             bw = new BufferedWriter(fw);
-                bw.write(toString());
+                bw.write(usuario.toString());
             bw.close();
         }
 
-        public void escribirObjetos() throws IOException{
+        public void escribirObjetos(Usuario contacto) throws IOException{
             File file = new File("AgendaObjetos.dat");
             if(!file.exists()){
                 file.createNewFile();
@@ -57,7 +34,7 @@ public class Agenda implements Serializable {
             try (DataOutputStream writer = new DataOutputStream(
                 new FileOutputStream(file));
                 ObjectOutputStream salida = new ObjectOutputStream(writer)){
-                    salida.writeObject(getNombre() + getApellidos() + getEmail() + getTelefono());
+                        salida.writeObject(contacto);   
                     writer.close();
             } catch (Exception e) {
             // TODO: handle exception
@@ -94,38 +71,12 @@ public class Agenda implements Serializable {
         }
         f.close();
         }
-      
-
-        //GETTERS Y SETTERS
-        public String getNombre() {
-            return nombre;
-        }
-        public void setNombre(String nombre) {
-            this.nombre = nombre;
-        }
-        public String getApellidos() {
-            return apellidos;
-        }
-        public void setApellidos(String apellidos) {
-            this.apellidos = apellidos;
-        }
-        public String getEmail() {
-            return email;
-        }
-        public void setEmail(String email) {
-            this.email = email;
-        }
-        public int getTelefono() {
-            return telefono;
-        }
-        public void setTelefono(int telefono) {
-            this.telefono = telefono;
-        }
 
         //MAIN
         public static void main(String[] args) {
             try {
             //Le pedimos al usuario sus datos
+            System.out.println("================");
             Scanner sc = new Scanner(System.in);
             System.out.print("Registrar Nombre: ");
             String nombre = sc.nextLine();
@@ -137,12 +88,13 @@ public class Agenda implements Serializable {
             int telefono = sc.nextInt();
             sc.close();
     
-            Agenda contacto = new Agenda(nombre,apellidos,email,telefono);
+            Agenda agenda = new Agenda();
+            Usuario contacto = new Usuario(nombre,apellidos,email,telefono);
             
-               contacto.escribirFichero();
-               contacto.listarAgenda();
-               contacto.escribirObjetos();
-               contacto.listarObjetos();
+            agenda.escribirFichero(contacto);
+            agenda.listarAgenda();
+            agenda.escribirObjetos(contacto);
+            agenda.listarObjetos();
             } catch (Exception e) {
                 //TODO: handle exception
                 System.out.println(e.getLocalizedMessage());
--- controles/controlAgenda/Usuario.java
+++ controles/controlAgenda/Usuario.java
@@ -1,3 +1,55 @@
+import java.util.ArrayList;
+
 public class Usuario {
-    
+     //Atributos
+     protected String nombre;
+    protected String apellidos;
+    protected String email;
+      protected int telefono;
+     //Constructor user pedido por terminal
+         public Usuario(String nombre, String apellidos, String email, int telefono) {
+             this.nombre = nombre;
+             this.apellidos = apellidos;
+             this.email = email;
+             this.telefono = telefono;
+         }
+     //CONSTRUCTOR MANUAL
+         public Usuario() {
+             this.nombre = "CambiarLuego";
+             this.apellidos = "CambiarLuego";
+             this.email = "CambiarLuego";
+             this.telefono = 000000000;
+         }
+
+         public String toString(){
+            String linea = " ";
+            linea += getNombre() + "\t" + getApellidos() + "\t" + getEmail() + "\t" + getTelefono() + "." + "\n";
+            return linea;
+        }
+         //GETTERS Y SETTERS
+         public String getNombre() {
+            return nombre;
+        }
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+        public String getApellidos() {
+            return apellidos;
+        }
+        public void setApellidos(String apellidos) {
+            this.apellidos = apellidos;
+        }
+        public String getEmail() {
+            return email;
+        }
+        public void setEmail(String email) {
+            this.email = email;
+        }
+        public int getTelefono() {
+            return telefono;
+        }
+        public void setTelefono(int telefono) {
+            this.telefono = telefono;
+        }
+     
 }
